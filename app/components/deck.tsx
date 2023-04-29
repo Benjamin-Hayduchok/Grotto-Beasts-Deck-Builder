6a72d7bd62e65d2494d73d767006d6784867264d
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-04-29 18:53:47 -0400

    ben-add: skeleton for side deck builder sticky

---
+++ app/components/deck.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import DeckHeader from './deckHeader'
+import DeckCards from './deckCards'
+
+type Props = {}
+
+type State = {}
+
+export default class deck extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+        <div className="deckSideBar">
+            <DeckHeader>
+            </DeckHeader>
+            <DeckCards>
+            </DeckCards>
+        </div>
+    )
+  }
+}
---
+++ app/components/deckHeader.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class deckHeader extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>deckHeader</div>
+    )
+  }
+}
