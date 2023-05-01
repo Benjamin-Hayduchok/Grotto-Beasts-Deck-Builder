927d939f8833e88dd2684801934c813d1e1706d7
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-05-01 16:37:35 -0400

    ben-add: images for cost and deckCard component

--- app/components/deckCard.tsx
+++ app/components/deckCard.tsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 
-type Props = {}
-
-type State = {}
-
-export default class deckCard extends Component<Props, State> {
-  state = {}
-
-  render() {
-    return (
-      <div>deckCard</div>
-    )
-  }
+const DeckCard = (props: {name: string, imageName: string, count: string, cost: string, isEpic: boolean}) => {
+  const { name, imageName, count, cost } = props;
+  console.log('"./cost" + cost + ".png"', "./cost" + cost + ".png")
+  return (
+    <div>
+      <img className="starIcon" src={"./cost+" + cost + ".png"}></img>
+      {name}
+    </div>
+  )
 }
+
+export default DeckCard
--- app/components/deckCards.tsx
+++ app/components/deckCards.tsx
@@ -5,15 +5,33 @@ type Props = {}
 
 type State = {}
 
+const currDeckArr = [
+  {cost: "0", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "1", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "2", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "3", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "4", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "5", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "6", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "7", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+];
+
 export default class deckCards extends Component<Props, State> {
   state = {}
 
   render() {
     return (
       <div className='deckCards'>
-        <DeckCard>
-
-        </DeckCard>
+          {currDeckArr.map(card => (
+              <DeckCard 
+                  name={card.name}
+                  imageName={card.imageName}
+                  count={card.count}
+                  cost={card.cost}
+                  isEpic={card.isEpic}
+              />
+          ))}
       </div>
     )
   }
