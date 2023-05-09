0274b9726f149ac3601e2e41248e4b42ef7dcf90
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-05-09 19:53:14 -0400

    ben-add: event bus for comp update + css updates

--- app/components/deckCards.tsx
+++ app/components/deckCards.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import DeckCard from './deckCard'
+import eventBus from './eventBus';
+
 
 type Props = {}
 
 type State = {}
 
-const currDeckArr = [
+var currDeckArr = [
   {cost: "0", name: "placeholder", imageName: "placeholder", count: "1", isEpic: true},
   {cost: "1", name: "Demon Lord Zeraxos", imageName: "placeholder", count: "1", isEpic: false},
   {cost: "2", name: "Maze of Many Ways", imageName: "placeholder", count: "1", isEpic: true},
@@ -14,13 +16,22 @@ const currDeckArr = [
   {cost: "5", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
   {cost: "6", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
   {cost: "7", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
-  {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
-  {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
-  {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
-  {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
-  {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  // {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  // {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  // {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  // {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
+  // {cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false},
 
 ];
+
+eventBus.on("couponApply", (data: any) =>
+        {
+            console.log("OMGGGGGG")
+            console.log(data)
+            currDeckArr.push({cost: "9", name: "placeholder", imageName: "placeholder", count: "1", isEpic: false})
+            console.log('currDeckArr', currDeckArr)
+        }
+    );
 
 export default class deckCards extends Component<Props, State> {
   state = {}
