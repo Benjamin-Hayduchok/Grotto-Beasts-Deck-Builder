7d10ec1b08077bede5c84375f7dd8ef452e991d4
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-05-12 22:32:01 -0400

    ben-add: logic to properly add card

--- app/components/deckCards.tsx
+++ app/components/deckCards.tsx
@@ -10,12 +10,37 @@ type State = {}
 
 var hack = true;
 
+const checkEpic = (cardType: string) => {
+  if (cardType[0] === "✦") return true;
+  return false;
+}
+
+const toStringInc = (input: string) => {
+  var toStringCount = parseInt(input);
+  toStringCount++;
+  return toStringCount.toString();
+}
+
 const getCardById = (id: string) =>{
   const cardListDict = cardList;
-  var test = cardListDict[id as keyof typeof cardListDict]
-  console.log('test', test)
-  // var test = cardListDict[id];
-  return cardListDict[id as keyof typeof cardListDict];
+  var cardFromDict = cardListDict[id as keyof typeof cardListDict];
+
+  console.log('cardFromDict :>> ', cardFromDict);
+  // mutate card { cost: string; name: string; imageName: string; count: string; isEpic: boolean; }': cost, name, imageName, count, isEpic
+  // name={card.name}
+  // imageName={card.imageName}
+  // count={card.count}
+  // cost={card.cost}
+  // isEpic={card.isEpic}
+  var returnCard = {
+    name: cardFromDict.name.toString(),
+    imageName: "placeholder, MUST REPLACE LATER!",
+    count: "1", // MUST GENERATE THIS LATER BY DETERMINING IF THE CARD EXISTS IN THE LIST YET
+    cost: cardFromDict.cost.toString(),
+    isEpic: checkEpic(cardFromDict.type)
+  };
+  
+  return returnCard;
 }
 
 var currDeckArr = [
@@ -44,14 +69,23 @@ export default function deckCards(props: any) {
     hack = false
     eventBus.on("addCardToDeck", (data: any) =>
       {
-        console.log('data', data)
-        var deckCopy = deckArr;
-        deckCopy.push(data.card);
-        setDeckArr([...deckCopy]);
+        console.log('data', data);;
+        var cardToAdd = getCardById(data.card.cardNum)
+        var isAddedToDeck = false;
+        for (var i = 0 ; i < deckArr.length; i++) {
+          var card = deckArr[i];
+          console.log('card', card)
+          if (cardToAdd.name === card.name) {
+            isAddedToDeck = true;
+            deckArr[i].count = toStringInc(deckArr[i].count);
+            break;
+          }
+        }
+        if (!isAddedToDeck) deckArr.push(cardToAdd);
+        setDeckArr([...deckArr]);
         hack = true;
         console.log('deckArrUPDATE ', deckArr)
         eventBus.remove("addCardToDeck")
-        getCardById(data.card.cardNum)
       }
     );  
   }
