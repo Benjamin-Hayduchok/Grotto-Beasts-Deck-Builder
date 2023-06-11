c93febd8bad51ec193c915b9e6ba45e25a1d2246
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-06-11 19:26:41 -0400

    ben-add: route for login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,28 +15,31 @@ export default function RootLayout({
     <html>
       <body>
         <main>
-        <nav className="navigation-menu navigation-container">
-          <a href="#" className="site-identity-logo">Grotto Beasts! - Decklist & Collection Tracker</a>
-          <input type="checkbox" id="toggleMenu" />
-          <label htmlFor="toggleMenu"><i className="ri-menu-line" id="toggleIcon"></i></label>
-          <section className="main-menu">
-            <ul className="navigation-menu__labels">
-              <li><a href="/collection">Collection</a></li>
-              <li><a href="/deckbuilder">Deck Builder</a></li>
-            </ul>
-          </section>
-        </nav>
+          <nav className="navigation-menu navigation-container">
+            <a href="#" className="site-identity-logo">Grotto Beasts! - Decklist & Collection Tracker</a>
+            <input type="checkbox" id="toggleMenu" />
+            <label htmlFor="toggleMenu"><i className="ri-menu-line" id="toggleIcon"></i></label>
+            <section className="main-menu">
+              <ul className="navigation-menu__labels">
+                <li><a href="/collection">Collection</a></li>
+                <li><a href="/deckbuilder">Deck Builder</a></li>
+              </ul>
+            </section>
+          </nav>
           <nav>
           <BrowserRouter>
             <Routes>
-            <Route path="/">  
-                  Home
-                </Route>
-                <Route path="/collection">
-                  Collection
-                </Route>
-                <Route path="/deckbuilder">
-                  Deck Builder
+              <Route path="/">  
+                Home
+              </Route>
+              <Route path="/login">  
+                Login 
+              </Route>
+              <Route path="/collection">
+                Collection  
+              </Route>
+              <Route path="/deckbuilder">
+                Deck Builder
               </Route>
             </Routes>
 
