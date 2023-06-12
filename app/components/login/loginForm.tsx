825ad41f6168e68c34b953e4f29e1731e230ff7b
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-06-11 22:13:34 -0400

    ben-add: API fetch calls to create users

--- app/components/login/loginForm.tsx
+++ app/components/login/loginForm.tsx
@@ -2,15 +2,28 @@ import React from 'react';
 import '../../../styles/login.css';
 
 export default function LoginForm(props: any) {
+    const login = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (typeof document !== 'undefined') {
+            const userObj = {
+              username: (document.getElementById("usernameRegister") as HTMLInputElement).value,
+              password: (document.getElementById("passwordRegister") as HTMLInputElement).value,
+              passwordConfirm: (document.getElementById("passwordConfirmRegister") as HTMLInputElement).value
+            }
+        }
+    }
     return (
-        <form className="login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>login</button>
+        <form className="login-form" onSubmit={login}>
+            <input type="text" placeholder="username" id="usernameRegister"/>
+            <input type="password" placeholder="password" id="passwordRegister"/>
+            <input type="password" placeholder="confirm password" id="passwordConfirmRegister"/>
+            <button>
+                login
+            </button>
             <p className="message">
                 Not registered? 
                 <a className="changeView" onClick={() => props.toggleView()}>
-                &nbsp;&nbsp;Create an account
+                    &nbsp;&nbsp;Create an account
                 </a>
             </p>
         </form>
--- app/components/login/registerForm.tsx
+++ app/components/login/registerForm.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import '../../../styles/login.css';
 
 export default function RegisterForm(props: any) {
+    async function createAccount(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        if (typeof document !== 'undefined') {
+            const userObj = {
+              username: (document.getElementById("usernameLogin") as HTMLInputElement).value,
+              password: (document.getElementById("passwordLogin") as HTMLInputElement).value,
+              passwordConfirm: (document.getElementById("passwordConfirmLogin") as HTMLInputElement).value
+            }
+            const response = await fetch("http://127.0.0.1:8090/api/collections/users/records", {   
+                method: "POST",
+                cache: "no-cache",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userObj)
+            });
+            console.log('response', response)
+        }
+    }
     return (
-        <form className="login-form">   
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>create</button>
+        <form className="login-form" onSubmit={createAccount}>   
+            <input type="text" placeholder="username" id="usernameLogin"/>
+            <input type="password" placeholder="password" id="passwordLogin"/>
+            <input type="password" placeholder="confirm password" id="passwordConfirmLogin"/>
+            <button>
+                create
+            </button>
             <p className="message">
                 Already registered?
                 <a className="changeView" onClick={() => props.toggleView()}>
