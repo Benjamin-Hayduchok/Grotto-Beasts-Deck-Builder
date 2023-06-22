659b5d2ae57670426594b7c943a8f7405a65c7c7
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-06-22 08:18:50 -0400

    Add modal provider wrapper to main content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Link from 'next/link';
-import './../styles/globals.css';
-import './../styles/nav.css';
-import { Route, Routes, BrowserRouter,  } from 'react-router-dom';
-
+import Link from "next/link";
+import "./../styles/globals.css";
+import "./../styles/nav.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { ModalProvider } from "./components/modalProvider/ModalProvider";
 
 export default function RootLayout({
   children,
@@ -15,20 +15,31 @@ export default function RootLayout({
     <html>
       <body>
         <main>
-          <nav className="navigation-menu navigation-container">
-            <a href="#" className="site-identity-logo">Grotto Beasts! - Decklist & Collection Tracker</a>
-            <input type="checkbox" id="toggleMenu" />
-            <label htmlFor="toggleMenu"><i className="ri-menu-line" id="toggleIcon"></i></label>
-            <section className="main-menu">
-              <ul className="navigation-menu__labels">
-                <li><a href="/collection">Collection</a></li>
-                <li><a href="/deckbuilder">Deck Builder</a></li>
-                <li><a href="/login">Login Page</a></li>
-              </ul>
-            </section>
-          </nav>
-          <nav>
-          {/* <BrowserRouter>
+          <ModalProvider>
+            <nav className="navigation-menu navigation-container">
+              <a href="#" className="site-identity-logo">
+                Grotto Beasts! - Decklist & Collection Tracker
+              </a>
+              <input type="checkbox" id="toggleMenu" />
+              <label htmlFor="toggleMenu">
+                <i className="ri-menu-line" id="toggleIcon"></i>
+              </label>
+              <section className="main-menu">
+                <ul className="navigation-menu__labels">
+                  <li>
+                    <a href="/collection">Collection</a>
+                  </li>
+                  <li>
+                    <a href="/deckbuilder">Deck Builder</a>
+                  </li>
+                  <li>
+                    <a href="/login">Login Page</a>
+                  </li>
+                </ul>
+              </section>
+            </nav>
+            <nav>
+              {/* <BrowserRouter>
             <Routes>
               <Route path="/">  
                 Home
@@ -45,10 +56,10 @@ export default function RootLayout({
             </Routes>
 
           </BrowserRouter> */}
-          </nav>
-          
-            
-          {children}
+            </nav>
+
+            {children}
+          </ModalProvider>
         </main>
       </body>
     </html>
