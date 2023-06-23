181b0150a0557eddaf6af05e15ae39b7e3825c84
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-06-23 09:31:43 -0400

    Card info carousel WIP

--- app/components/cardInfo/cardInfoCarousel.tsx
+++ app/components/cardInfo/cardInfoCarousel.tsx
@@ -1,47 +1,102 @@
-import { FC, useContext, useState } from "react";
-import { CardDataContext } from "../providers/cardDataProvider/CardDataProvider";
+import { FC, useContext, useEffect, useState } from "react";
+import {
+  CardDataContext,
+  CardsData,
+} from "../providers/cardDataProvider/CardDataProvider";
+import classNames from "classnames";
+import { Card } from "../tailwindComponents/card/card";
 
 export type CardInfoCarouselProps = {
   cardNum: number;
 };
 
 export const CardInfoCarousel: FC<CardInfoCarouselProps> = ({ cardNum }) => {
+  const cardsData = useContext(CardDataContext);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const cardsData  = useContext(CardDataContext);
-  console.log('cardsData', cardsData)
+  const [currentItem, setCurrentItem] = useState<CardsData>();
 
-  const handlePrevious = () => {
+  useEffect(() => {
+    if (cardsData) {
+      setCurrentItem(cardsData[currentIndex]);
+    }
+  }, [currentIndex, cardsData]);
+
+  console.log("Current item", currentItem);
+
+  const handlePrevious = (data: CardsData[]) => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? items.length - 1 : prevIndex - 1
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1
     );
   };
 
-  const handleNext = () => {
+  const handleNext = (data: CardsData[]) => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === items.length - 1 ? 0 : prevIndex + 1
+      prevIndex === data.length - 1 ? 0 : prevIndex + 1
     );
   };
 
-  const currentItem = items[currentIndex];
+  const baseClasses = (classes: string) => {
+    return classes;
+  };
+  const desktopClasses = (classes: string) => {
+    const classList = classes.split(" ");
+    const desktopClassList = classList.map((className) => `md:${className}`);
+    return desktopClassList.join(" ");
+  };
 
   return (
-    <div className="flex items-center justify-center">
-      <button
-        className="bg-gray-200 rounded-full p-2 mr-2"
-        onClick={handlePrevious}
-      >
-        Previous
-      </button>
-      <div className="border border-gray-200 p-4">
-        <h2 className="text-lg font-semibold mb-2">{currentItem.title}</h2>
-        <p>{currentItem.description}</p>
-      </div>
-      <button
-        className="bg-gray-200 rounded-full p-2 ml-2"
-        onClick={handleNext}
-      >
-        Next
-      </button>
-    </div>
+    <>
+      {cardsData && currentItem && (
+        <div
+          className={classNames(
+            "flex flex-row",
+            "items-center justify-center",
+            "text-slate-50"
+          )}
+        >
+          <button
+            className="bg-gray-200 rounded-full p-2 mr-2"
+            onClick={() => handlePrevious(cardsData)}
+          >
+            Previous
+          </button>
+          <div
+            className={classNames(
+              baseClasses(
+                classNames(
+                  "flex flex-col",
+                  "gap-16 border",
+                  "border-gray-200 p-4"
+                )
+              ),
+              desktopClasses("flex-row")
+            )}
+          >
+            <Card
+              name={currentItem.name}
+              cardNum={currentItem.cardNum}
+              imageName={currentItem.imageName}
+              effect={currentItem.effect}
+              collectionView={false}
+              showInfoButton={false}
+              glow={{ show: false }}
+              cardDimensions={{ maxWidth: "max-w-[300px]" }}
+            />
+            <div className={classNames("flex flex-col gap-4")}>
+              <h2 className="text-lg font-semibold mb-2">
+                {currentItem?.name}
+              </h2>
+              <p>{currentItem?.flavorText}</p>
+            </div>
+          </div>
+          <button
+            className="bg-gray-200 rounded-full p-2 ml-2"
+            onClick={() => handleNext(cardsData)}
+          >
+            Next
+          </button>
+        </div>
+      )}
+    </>
   );
 };
