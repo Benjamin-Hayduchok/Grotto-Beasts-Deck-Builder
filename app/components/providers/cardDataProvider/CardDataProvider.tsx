1d6d43632d83f002ba7af7d5558a377ea878a644
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-06-23 09:28:41 -0400

    Change card provider data to array

--- app/components/providers/cardDataProvider/CardDataProvider.tsx
+++ app/components/providers/cardDataProvider/CardDataProvider.tsx
@@ -7,32 +7,28 @@ import {
 } from "react";
 import allCards from "../../card-list.json";
 
-type CardsData = {
-  cardsData: {
-    [key: number]: {
-      name: string;
-      power: any;
-      goal: any;
-      cost: any;
-      effect: string;
-      flavorText: string;
-      type: string;
-      artist: string;
-      imageName: string;
-      deckCardImage: string;
-      cardNum: string;
-    };
-  };
+export type CardsData = {
+  name: string;
+  power: any;
+  goal: any;
+  cost: any;
+  effect: string;
+  flavorText: string;
+  type: string;
+  artist: string;
+  imageName: string;
+  deckCardImage: string;
+  cardNum: string;
 };
 
-export const CardDataContext = createContext<CardsData | undefined>(undefined);
+export const CardDataContext = createContext<CardsData[] | undefined>(
+  undefined
+);
 
 export const CardDataProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [allCardsData, setAllCardsData] = useState<CardsData>({
-    cardsData: allCards,
-  });
-
-  console.log(allCardsData);
+  const [allCardsData, setAllCardsData] = useState<CardsData[]>(
+    Object.values(allCards)
+  );
 
   return (
     <CardDataContext.Provider value={allCardsData}>
