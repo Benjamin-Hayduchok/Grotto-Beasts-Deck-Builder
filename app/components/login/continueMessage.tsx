4d4b93551b5080e100cf63a161c1178d90fb8e0a
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-06-26 21:32:40 -0400

    ben-add: temp continue message, to delete later when confirmed not needed

---
+++ app/components/login/continueMessage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classNames from "classnames";
+
+export default function ContinueMessage() {
+    return (
+        <p
+        className={classNames(
+            "flex m-auto"
+        )}
+    >
+        Continue with Google
+    </p>
+    )
+}
