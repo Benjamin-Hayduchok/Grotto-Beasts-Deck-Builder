abb9cee43a62bb5bb07c298dd3c4c396578b43c4
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-06-28 19:28:44 -0400

    ben-clean: code to look nicer

--- app/components/login/oAuth2Login.tsx
+++ app/components/login/oAuth2Login.tsx
@@ -7,19 +7,10 @@ export default function OAuth2Login() {
     return (
         <div
             className={classNames(
-                "container mx-auto",
-                "w-1/4",
-                "h-[50vh]",
-                "rounded-[1vw]",
-                "items-center",
-                "bg-gray-200",
-                "flex-col",
-                "justify-center",
-                "absolute",
-                "top-1/2",
-                "left-1/2",
-                "-translate-y-1/2",
-                "-translate-x-1/2"
+                "container mx-auto w-1/4 h-[50vh]",
+                "bg-gray-200 rounded-[1vw]",
+                "flex-col items-center justify-center",
+                "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
             )}
         >   
             <LoginHeader/>
