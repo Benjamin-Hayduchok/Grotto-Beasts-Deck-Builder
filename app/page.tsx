4ab23a75ec9e2d09c835a49fe85e835d2f787bf3
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-06-28 21:04:08 -0400

    ben-update: home endpoint to redirect to a new deckbuilder

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,17 @@ import StickyBox from "react-sticky-box";
 import Deck from './components/deck'
 
 export default function HomePage() {
-    return (
-        <div>
-            home page
-            <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
-                <Deck collectionView={false}>
-                </Deck>
-            </StickyBox>
-            <br></br>
-            <CardList collectionView={false}></CardList>
-        </div>
-    )
+    window.location.href = new URL(window.location.href).origin + "/deckbuilder?new";
+    // Will likely add something here once the stack has enough to display for a homepage if decided upon
+    // return (
+    //     <div>
+    //         home page
+    //         <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
+    //             <Deck collectionView={false}>
+    //             </Deck>
+    //         </StickyBox>
+    //         <br></br>
+    //         <CardList collectionView={false}></CardList>
+    //     </div>
+    // )
 }
