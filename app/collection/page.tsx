885a3039b36a587523754bcb1c5a5c4a821fb289
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-06-29 14:32:41 -0400

    ben-update: collection endpoints to allow wildcards
    necessary to ensure when a new user who is not logged in has a way to track that it is a new, unregistered deckbuilder. new tag will be used to know to save the deck in local storage and then save later when they login/make a new account. Forces users to go to a new collection if they are not signed in, so logic needs to be in place on load for collection/deckbuilder to check if they are logged in or not

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -6,20 +6,21 @@ import Deck from '../components/deck';
 import PocketBase from 'pocketbase';
 
 const CollectionPage = (props: any) => {
-    const pb = new PocketBase('https://grotto-beasts-test.fly.dev'); 
-    console.log(pb.authStore.token);
+    if (typeof window !== "undefined") window.location.href = new URL(window.location.href).origin + "/collection/new";
+    // const pb = new PocketBase('https://grotto-beasts-test.fly.dev'); 
+    // console.log(pb.authStore.token);
 
 
-    return (
-        <div>
-            <br></br>
-            <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
-                <Deck collectionView={true}>
-                </Deck>
-            </StickyBox>
-            <CardList collectionView={true}></CardList>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <br></br>
+    //         <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
+    //             <Deck collectionView={true}>
+    //             </Deck>
+    //         </StickyBox>
+    //         <CardList collectionView={true}></CardList>
+    //     </div>
+    // )
 }
 
 export default CollectionPage;
