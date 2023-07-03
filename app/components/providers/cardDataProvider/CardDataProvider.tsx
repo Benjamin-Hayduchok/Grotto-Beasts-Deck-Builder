af7977cf6a96840635943985be1eada7639be917
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-07-03 14:47:26 -0400

    ben-add: logic to load from collection

--- app/components/providers/cardDataProvider/CardDataProvider.tsx
+++ app/components/providers/cardDataProvider/CardDataProvider.tsx
@@ -2,10 +2,12 @@ import {
   FC,
   PropsWithChildren,
   createContext,
+  useContext,
   useEffect,
   useState,
 } from "react";
 import allCards from "../../card-list.json";
+import { PocketBaseContext } from "../pocketBaseProvider/PocketBaseProvider";
 
 export type CardsData = {
   name: string;
@@ -27,12 +29,30 @@ export const CardDataContext = createContext<CardsData[] | undefined>(
 );
 
 export const CardDataProvider: FC<PropsWithChildren> = ({ children }) => {
+  const pocketBaseConnection = useContext(PocketBaseContext);
   const [allCardsData, setAllCardsData] = useState<CardsData[]>(
     Object.values(allCards)
   );
 
+  const updateCollectionCount = (cardList: CardsData[] | undefined) => {
+    // hit db and get the users collection values to update collection within the context
+    var collectionCountDict: {
+      [collectionCount: number]: number
+    } = {
+          0:1,
+          1:3,
+          4:2,
+          5:0 
+        };
+    for (var collectionCount in collectionCountDict) {
+      var currCard = cardList?.at(parseInt(collectionCount));
+      if (currCard) currCard.collectionCount = collectionCountDict[collectionCount];
+    }
+    if (!pocketBaseConnection?.authStore.isValid) return cardList;
+  }
+
   return (
-    <CardDataContext.Provider value={allCardsData}>
+    <CardDataContext.Provider value={updateCollectionCount(allCardsData)}>
       {children}
     </CardDataContext.Provider>
   );
