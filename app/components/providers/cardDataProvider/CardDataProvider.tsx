cbc7342ee3ea4b6597e3e36686fd1a9f55b3acf1
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-07-04 21:06:37 -0400

    ben-wip: collection loading from db
    good progress made already, just need to finalize functionality and clean it up

--- app/components/providers/cardDataProvider/CardDataProvider.tsx
+++ app/components/providers/cardDataProvider/CardDataProvider.tsx
@@ -24,6 +24,19 @@ export type CardsData = {
   collectionCount: number;
 };
 
+export type collectCountData = {
+  [collectionCount: number]: number
+} | undefined;
+
+const getCollectionCountFromDB = async () => { // should be used eventually
+  return{
+    0:1,
+    1:3,
+    4:2,
+    5:0 
+  }
+}
+
 export const CardDataContext = createContext<CardsData[] | undefined>(
   undefined
 );
@@ -33,6 +46,43 @@ export const CardDataProvider: FC<PropsWithChildren> = ({ children }) => {
   const [allCardsData, setAllCardsData] = useState<CardsData[]>(
     Object.values(allCards)
   );
+  const [collectionCounts, setCollectionCounts] = useState<collectCountData>(
+    undefined // might want to change later
+  )
+
+  useEffect(() => {
+    const getCollectionCountData = async () => {
+      // if you are in collection, check the path param.
+      // use path param to load collection which should be the id of the collection itself, if new path param, load 0s for all collection counts
+      // if you are not in collection, check local storage values to see if you have collectionId
+      // if you have collectionId, get collectionId stored in local storage and load cards that way
+      // otherwise, load 0 into all cards
+      var path = window.location.href;
+      var split = path.split("/");
+      if (split.length >= 5) {
+        if (split[3] === "collection") {
+          var id = split[4];
+          const record = await pocketBaseConnection?.collection('cardCollection').getOne(id);
+          var collectionCount = record?.collection;
+          setCollectionCounts(collectionCount);
+          return;
+        }
+        if (true)  {// if collectionId in localStorage
+          return // pocketbase collection value
+        }
+        return // all zeros
+      }
+
+      if (pocketBaseConnection?.authStore.isValid) {
+        pocketBaseConnection?.autoCancellation(false);
+        const resultList = await pocketBaseConnection?.collection('cardCollection').getList(1, 50); // need to add filter based on user log in
+        pocketBaseConnection?.autoCancellation(true);
+        console.log('resultList', resultList) // work back here
+      }
+      // setCollectionCount(undefined);
+    }
+    getCollectionCountData();
+  }, []);
 
   const updateCollectionCount = (cardList: CardsData[] | undefined) => {
     // hit db and get the users collection values to update collection within the context
@@ -44,10 +94,19 @@ export const CardDataProvider: FC<PropsWithChildren> = ({ children }) => {
           4:2,
           5:0 
         };
-    for (var collectionCount in collectionCountDict) {
+
+    var stringTest = JSON.stringify(collectionCountDict)
+    var parseTest = JSON.parse(stringTest); // DELETE LATER, USED TO CREATE JSON OBJECTS THAT WORK IN DB FOR SETUP
+
+
+    // should be its own func
+    for (var collectionCount in collectionCounts) {
       var currCard = cardList?.at(parseInt(collectionCount));
-      if (currCard) currCard.collectionCount = collectionCountDict[collectionCount];
+      if (currCard) currCard.collectionCount = collectionCounts[parseInt(collectionCount)];
     }
+    return cardList;
+
+
     if (!pocketBaseConnection?.authStore.isValid) return cardList;
   }
 
