185fa437be891ab46f7592fdb286d207bd0a9e06
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-07-07 14:08:04 -0400

    ben-fix: deploy bug not including cardList in cardArray prop

--- app/deckbuilder/[id]/page.tsx
+++ app/deckbuilder/[id]/page.tsx
@@ -3,8 +3,15 @@
 import CardList from '../../components/cardList';
 import StickyBox from "react-sticky-box";
 import Deck from '../../components/deck'
+import { useContext, useEffect, useState } from "react";
+import {
+    CardDataContext,
+} from "../../components/providers/cardDataProvider/CardDataProvider";
 
 export default function DeckBuilder() {
+    const cardsData = useContext(CardDataContext);
+    const [cardList, setCardList] = useState(Object.assign({}, cardsData));
+
     return (
         <div>
             <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
@@ -12,7 +19,7 @@ export default function DeckBuilder() {
                 </Deck>
             </StickyBox>
             <br></br>
-            <CardList collectionView={false}></CardList>
+            <CardList collectionView={true} cardArray={Object.values(cardList)}></CardList>
         </div>
     )
 }
