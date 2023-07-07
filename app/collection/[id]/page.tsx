41193b7bace8208c725bd3b8c06aca3711ed11c3
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-07-06 20:08:26 -0400

    ben-add: load collection logic to collection page
    kinda no idea how this updates the card collection on its own, kinda weird

--- app/collection/[id]/page.tsx
+++ app/collection/[id]/page.tsx
@@ -3,12 +3,35 @@
 import CardList from '../../components/cardList';
 import StickyBox from "react-sticky-box";
 import Deck from '../../components/deck';
-import { PocketBaseContext } from "../../components/providers/pocketBaseProvider/PocketBaseProvider";
 import { useContext } from "react";
+import {
+    CardDataContext,
+} from "../../components/providers/cardDataProvider/CardDataProvider";
 
-const CollectionPage = ({ params }: any) => {
-    console.log('params.id', params.id);    
-    const pocketBaseConnection = useContext(PocketBaseContext);
+const setCollectionCounts = (collectionCounts: any, cardsData: any) => {
+    for (var collectionCount in collectionCounts) {
+      var currCard = cardsData?.at(parseInt(collectionCount));
+      if (currCard) currCard.collectionCount = collectionCounts[parseInt(collectionCount)];
+    }
+    return cardsData;
+  }
+
+async function getCollectionCount(id: string, cardsData: any) {
+    const res = await fetch(
+        `https://grotto-beasts-test.fly.dev/api/collections/cardCollection/records/${id}`,
+        {
+          next: { revalidate: 10 },
+        }
+    );
+    const data = await res.json();
+
+    setCollectionCounts(data?.collection, cardsData);
+    return data?.collection;
+}
+
+export default async function CollectionPage({ params }: any) {
+    const cardsData = useContext(CardDataContext);
+    await getCollectionCount(params.id, cardsData);
 
     return (
         <div>
@@ -21,5 +44,3 @@ const CollectionPage = ({ params }: any) => {
         </div>
     )
 }
-
-export default CollectionPage;
