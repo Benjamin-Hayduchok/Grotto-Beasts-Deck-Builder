847bde98894a0ca22e90c16bbda612235bd960a9
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-07-07 09:23:23 -0400

    small fix part 2 -- and added searchbar to collection

--- app/collection/[id]/page.tsx
+++ app/collection/[id]/page.tsx
@@ -1,25 +1,24 @@
 "use client";
 
-import CardList from '../../components/cardList';
+import CardList from "../../components/cardList";
 import StickyBox from "react-sticky-box";
-import Deck from '../../components/deck';
-import PocketBase from 'pocketbase';
+import Deck from "../../components/deck";
+import PocketBase from "pocketbase";
+import SearchBar from "@/app/components/searchBar";
 
 const CollectionPage = (props: any) => {
-    const pb = new PocketBase('https://grotto-beasts-test.fly.dev'); 
-    console.log(pb.authStore.token);
+  const pb = new PocketBase("https://grotto-beasts-test.fly.dev");
+  console.log(pb.authStore.token);
 
-
-    return (
-        <div>
-            <br></br>
-            <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
-                <Deck collectionView={true}>
-                </Deck>
-            </StickyBox>
-            <CardList collectionView={true}></CardList>
-        </div>
-    )
-}
+  return (
+    <div>
+      <SearchBar></SearchBar>
+      <StickyBox className="deckSticky" offsetTop={20} offsetBottom={20}>
+        <Deck collectionView={true} />
+      </StickyBox>
+      <CardList collectionView={true}></CardList>
+    </div>
+  );
+};
 
 export default CollectionPage;
