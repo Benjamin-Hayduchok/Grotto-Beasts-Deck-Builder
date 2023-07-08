e1f17e24f23e158b6cded44c4b7ea32237c89aca
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-07-08 17:54:00 -0400

    ben-removed: href in button

--- app/components/saveButton.tsx
+++ app/components/saveButton.tsx
@@ -9,7 +9,7 @@ export type SaveButtonProps = {
 
 export const SaveButton: FC<SaveButtonProps> = ({ save }) => {
     return (
-        <a href="#_" 
+        <a 
             className={classNames(
                 "flex absolute mt-6 justify-center rounded w-40 h-14 px-5 py-2.5",
                 "overflow-hidden group bg-green-500 hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300"
