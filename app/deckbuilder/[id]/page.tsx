a7d79b3c913f5e9ec6330c59383ed1572d30c651
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-07-09 17:15:55 -0400

    Okay, actually fixed the build issue

--- app/deckbuilder/[id]/page.tsx
+++ app/deckbuilder/[id]/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import CardList from '../../components/cardList';
+import CardList from "../../components/cardList";
 import StickyBox from "react-sticky-box";
-import Deck from '../../components/deck'
+import Deck from "../../components/deck";
 
 export default function DeckBuilder() {
-    return (
-        <div>
-            <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
-                <Deck collectionView={false}>
-                </Deck>
-            </StickyBox>
-            <br></br>
-            <CardList collectionView={false}></CardList>
-        </div>
-    )
+  return (
+    <div>
+      <StickyBox className="deckSticky" offsetTop={20} offsetBottom={20}>
+        <Deck collectionView={false} />
+      </StickyBox>
+      <CardList collectionView={false}></CardList>
+    </div>
+  );
 }
