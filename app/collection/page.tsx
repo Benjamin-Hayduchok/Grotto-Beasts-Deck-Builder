ea8724659b8aa8eb9441ee160118c709bcc0b553
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-07-26 13:16:46 -0400

    ben-update: redirect logic to be more concise and readable

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -3,29 +3,24 @@
 import { PocketBaseContext } from "../components/providers/pocketBaseProvider/PocketBaseProvider";
 import { useContext } from "react";
 
-const CollectionPage = (props: any) => {
+const CollectionPage = () => {
     const pocketBaseConnection = useContext(PocketBaseContext);
     console.log('pocketBaseConnection', pocketBaseConnection);
 
-    if (typeof window !== "undefined" && pocketBaseConnection?.authStore.isValid) {
+    if (typeof window === "undefined") {
+        return;
+    }
+
+    const { authStore } = pocketBaseConnection || {};
+    if (authStore?.isValid) {
         const collectionId = localStorage.getItem('collectionCountId');
-        if (collectionId !== null && collectionId.length > 0) {
-            window.location.href = new URL(window.location.href).origin + "/collection/" + collectionId;
-            return;
+        if (collectionId && collectionId.length > 0) {
+        window.location.href = new URL(window.location.href).origin + "/collection/" + collectionId;
+        return;
         }
     }
-    if (typeof window !== "undefined") window.location.href = new URL(window.location.href).origin + "/collection/new";
-
-    // return (
-    //     <div>
-    //         <br></br>
-    //         <StickyBox className='deckSticky' offsetTop={20} offsetBottom={20}>
-    //             <Deck collectionView={true}>
-    //             </Deck>
-    //         </StickyBox>
-    //         <CardList collectionView={true}></CardList>
-    //     </div>
-    // )
+    
+    window.location.href = new URL(window.location.href).origin + "/collection/new";
 }
 
 export default CollectionPage;
