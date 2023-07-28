6a80ddff97186914efd0f37e089065714311b020
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-07-27 21:11:37 -0400

    address build warnings: img alt text, useEffect deps

--- app/components/collectionCardHover.tsx
+++ app/components/collectionCardHover.tsx
@@ -1,44 +1,44 @@
-  import React, { useState, useEffect } from "react";
-import './../../styles/collection.css';
-import CardPlusButtom from './cardPlusButton';
-import CardMinusButton from './cardMinusButton';
-import CollectionCardCount from './collectionCardCount'
+import React, { useState, useEffect } from "react";
+import "./../../styles/collection.css";
+import CardPlusButtom from "./cardPlusButton";
+import CardMinusButton from "./cardMinusButton";
+import CollectionCardCount from "./collectionCardCount";
 
+export default function CollectionCardHover(props: {
+  collectionView: boolean;
+  collectionCount: number;
+  cardNum: string;
+  updateCollectionCount: Function;
+}) {
+  const [cardCount, setCardCount] = useState(props.collectionCount);
+  const [init, setInit] = useState(false);
 
+  useEffect(() => {
+    if (!init) setCardCount(props.collectionCount);
+  }, [init, props.collectionCount]);
 
-export default function CollectionCardHover(props: {collectionView: boolean, collectionCount: number, cardNum: string, updateCollectionCount: Function}) {
-    const [cardCount, setCardCount] = useState(props.collectionCount);
-    const [init, setInit] = useState(false);
+  const plusPressed = () => {
+    setCardCount(cardCount + 1);
+    setInit(true);
+    props.updateCollectionCount(true, props.cardNum);
+  };
 
-    useEffect(() => {
-        if (!init) setCardCount(props.collectionCount);
-    }) 
-
-    const plusPressed = () => {
-        setCardCount(cardCount + 1);
-        setInit(true);
-        props.updateCollectionCount(true, props.cardNum)
+  const minusPressed = () => {
+    if (cardCount > 0) {
+      setCardCount(cardCount - 1);
+      setInit(true);
+      props.updateCollectionCount(false, props.cardNum);
     }
+  };
 
-    const minusPressed = () => {
-        if (cardCount > 0) {
-            setCardCount(cardCount - 1);
-            setInit(true);
-            props.updateCollectionCount(false, props.cardNum)
-        }
-    }
-
-    if (props.collectionView) {
-        return (
-            <div className='collectionCardHoverContainer'>
-                <CardMinusButton cardDec={minusPressed}/>
-                <CollectionCardCount count={cardCount.toString()}/>
-                <CardPlusButtom cardInc={plusPressed}/>
-            </div>
-        )
-    }
+  if (props.collectionView) {
     return (
-        <div>
-        </div>
-    )
+      <div className="collectionCardHoverContainer">
+        <CardMinusButton cardDec={minusPressed} />
+        <CollectionCardCount count={cardCount.toString()} />
+        <CardPlusButtom cardInc={plusPressed} />
+      </div>
+    );
+  }
+  return <div></div>;
 }
