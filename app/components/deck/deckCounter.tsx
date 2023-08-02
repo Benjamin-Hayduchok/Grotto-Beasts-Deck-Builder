ce2dcafc0edca07574853e9dc74524594031dd23
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-08-01 22:40:23 -0400

    ben-add: logic to show deck count and max count

--- app/components/deck/deckCounter.tsx
+++ app/components/deck/deckCounter.tsx
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import eventBus from "../eventBus";
 import util from "../util";
 import Swal from "sweetalert2";
 import classNames from "classnames";
+import { DeckListContext } from "../providers/deckListProvider/DeckListProvider";
 
 export default function DeckCounter(props: { collectionView: boolean }) {
-  const [deckListCount, setDeckListCount] = useState("0");
-  const [cardCount, setCardCount] = useState("0");
-  const [maxCount, setMaxCount] = useState("40"); // used specifically for byeah prime
+  const { challenger, deckListLength } = useContext(DeckListContext);
+  const [deckListCount, setDeckListCount] = useState(deckListLength);
+  const [maxCount, setMaxCount] = useState(
+    challenger === "Byeah Prime" ? 60 : 40
+  ); // used specifically for byeah prime
+
+  useEffect(() => {
+    setDeckListCount(deckListLength);
+    setMaxCount(challenger === "Byeah Prime" ? 60 : 40);
+  }, [challenger, deckListLength]);
 
   if (props.collectionView) {
     return (
@@ -17,34 +25,11 @@ export default function DeckCounter(props: { collectionView: boolean }) {
     );
   }
 
-  eventBus.on("incrementDeckCounter", (data: any) => {
-    setCardCount(util.toStringInc(cardCount));
-  });
-  eventBus.on("decrementDeckCounter", (data: any) => {
-    setCardCount(util.toStringDec(cardCount));
-  });
-  eventBus.on("addChallengerToDeck", (data: any) => {
-    const challenger = data.card.name;
-    var tempMax = "40";
-    if (challenger === "Byeah Prime") {
-      setMaxCount("60");
-      tempMax = "60";
-    } else setMaxCount("40");
-    if (parseInt(cardCount) > parseInt(tempMax)) {
-      Swal.fire({
-        title: "<strong>Warning!</strong>",
-        html: "<b>You currently have more cards than the maximum allowed cards. Please remove extra cards.</b>",
-        icon: "error",
-        confirmButtonColor: "#257d52",
-        confirmButtonText: "Thank you!",
-      });
-    }
-  });
   return (
     <div className={classNames("flex justify-between text-lg ")}>
       Cards:
       <span>
-        {cardCount}/{maxCount}
+        {deckListCount}/{maxCount}
       </span>
     </div>
   );
