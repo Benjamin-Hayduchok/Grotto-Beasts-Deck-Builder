ab0986b0d40ea66f83641bff0f7e42b760d52c3b
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-08-01 20:00:43 -0400

    ben-add: logic for epic ruling
    only one epic at a time unless jex is in deck. Added state value for current epics in an epic array, will need to update array when removing eventually

--- app/components/providers/deckListProvider/DeckListProvider.tsx
+++ app/components/providers/deckListProvider/DeckListProvider.tsx
@@ -1,116 +1,150 @@
 import {
-    FC,
-    PropsWithChildren,
-    createContext,
-    useState,
-    useContext,
-    useReducer
-  } from "react";
-  import util from "../../util";
-  import { CardDataContext } from "../cardDataProvider";
-  
-  export type DeckListType = {
-    cost: string;
-    cardNum: string;
-    name: string;
-    imageName: string;
-    count: string;
-    isEpic: boolean;
-  };
+  FC,
+  PropsWithChildren,
+  createContext,
+  useState,
+  useContext,
+  useReducer,
+} from "react";
+import util from "../../util";
+import { CardDataContext } from "../cardDataProvider";
+import Swal from "sweetalert2";
 
-  type DeckListContextType = {
-    deckList: DeckListType[] | undefined;
-    challenger: string,
-    deckListLength: number,
-    addToDeckList: (cardNum: string) => void;
-    removeFromDeckList: (cardNum: string) => void;
-    forceRender: boolean;
-    forceRenderDispatch: () => void;
-  };
-  
-  export const DeckListContext = createContext<DeckListContextType>({
-        deckList: undefined,
-        challenger: "None",
-        deckListLength: 0,
-        addToDeckList: () => {},
-        removeFromDeckList: () => {},
-        forceRender: false,
-        forceRenderDispatch: () => {}
-  });
-  
-  export const DeckListProvider: FC<PropsWithChildren> = ({ children }) => {
-    const [deckList, setDeckList] = useState<DeckListType[]>(
-      [] // should be populated with API call to get decklist
-    );
-    const [challenger, setChallenger] = useState<string>(
-        "None" // should be populated with API call to get decklist
-      );
-      const [deckListLength, setDeckListLength] = useState<number>(
-        0 // should be populated with API call to get decklist
-      );
-    const [forceRender, forceRenderDispatch] = useReducer((state) => !state, false);
-    const { cardsData } = useContext(CardDataContext);
+export type DeckListType = {
+  cost: string;
+  cardNum: string;
+  name: string;
+  imageName: string;
+  count: string;
+  isEpic: boolean;
+};
 
-    const addToDeckList = (cardNum: string) => {
-        console.log("context adding")
-        if (typeof cardsData === "undefined" || typeof deckList === "undefined") {
-            return;
-        }
-        var cardObj = cardsData[parseInt(cardNum) - 1];
+type DeckListContextType = {
+  deckList: DeckListType[] | undefined;
+  challenger: string;
+  deckListLength: number;
+  epicArray: String[];
+  addToDeckList: (cardNum: string) => void;
+  removeFromDeckList: (cardNum: string) => void;
+  forceRender: boolean;
+  forceRenderDispatch: () => void;
+};
 
-        if (cardObj && parseInt(cardObj.cardNum) <= 32) { // checking if card is challenger to set
-            setChallenger(cardObj.name);
-            return; // nothing else to be added, just updating challenger
-        }
-        var copyDeckList = [...deckList];
-        
-        var deckListLimit = challenger === "Byeah Prime" ? 60 : 40;
+export const DeckListContext = createContext<DeckListContextType>({
+  deckList: undefined,
+  challenger: "None",
+  deckListLength: 0,
+  epicArray: [],
+  addToDeckList: () => {},
+  removeFromDeckList: () => {},
+  forceRender: false,
+  forceRenderDispatch: () => {},
+});
 
-        if (deckListLength >= deckListLimit) {
-            return; // BEN NOTE: MIGHT UPDATE THIS LATER TO SHOW SWAL IF CHANGING FROM BYEAH PRIME
-        }
+export const DeckListProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [deckList, setDeckList] = useState<DeckListType[]>(
+    [] // should be populated with API call to get decklist
+  );
+  const [challenger, setChallenger] = useState<string>(
+    "None" // should be populated with API call to get decklist
+  );
+  const [deckListLength, setDeckListLength] = useState<number>(
+    0 // should be populated with API call to get decklist
+  );
+  const [epicArray, setEpicArray] = useState<String[]>(
+    [] // should be populated with API call to get decklist
+  );
+  const [forceRender, forceRenderDispatch] = useReducer(
+    (state) => !state,
+    false
+  );
+  const { cardsData } = useContext(CardDataContext);
 
-        for (var index in copyDeckList) {
-            var deckCard = copyDeckList[index];
-            if (cardNum === deckCard.cardNum) {
-                if (deckCard.count !== "3" || cardObj.name === "Byeah Beast") { // can have unlimited byeah beast 
-                    copyDeckList[index].count = util.toStringInc(copyDeckList[index].count);
-                    setDeckListLength(deckListLength + 1);
-                }
-                return;
-            }
-        }
-        var parsedCard = {
-            cost: cardObj.cost,
-            cardNum: cardObj.cardNum,
-            name: cardObj.name,
-            imageName: cardObj.deckCardImage,
-            count: "1",
-            isEpic: cardObj.type[0] === '✦'
-        }
-        copyDeckList.push(parsedCard);
-        setDeckListLength(deckListLength + 1);
-        setDeckList([...copyDeckList]);
+  const addToDeckList = (cardNum: string) => {
+    if (typeof cardsData === "undefined" || typeof deckList === "undefined") {
+      return;
+    }
+    var cardObj = cardsData[parseInt(cardNum) - 1];
+
+    if (cardObj && parseInt(cardObj.cardNum) <= 32) {
+      // checking if card is challenger to set
+      setChallenger(cardObj.name);
+      return; // nothing else to be added, just updating challenger
+    }
+    var copyDeckList = [...deckList];
+
+    var deckListLimit = challenger === "Byeah Prime" ? 60 : 40;
+
+    if (deckListLength >= deckListLimit) {
+      Swal.fire({
+        title: "<strong>Card Limit Reached</strong>",
+        html: `<b>Can only have ${deckListLimit} cards in your deck.</b>`,
+        icon: "error",
+        confirmButtonColor: "#f27474",
+        confirmButtonText: "Close",
+      });
+      return; // BEN NOTE: MIGHT UPDATE THIS LATER TO SHOW SWAL IF CHANGING FROM BYEAH PRIME
+    }
+    if (
+      epicArray.length !== 0 &&
+      challenger !== "JEX" &&
+      !epicArray.includes(cardObj.cardNum)
+    ) {
+      Swal.fire({
+        title: "<strong>Cannot add more than one Epic without JEX</strong>",
+        icon: "error",
+        confirmButtonColor: "#f27474",
+        confirmButtonText: "Close",
+      });
+      return; // can't have more than one epic if you don't have jex
     }
 
-    const removeFromDeckList = (cardNum: string) => {
-        console.log("context remove hit")
+    for (var index in copyDeckList) {
+      // attempting to add card to decklist
+      var deckCard = copyDeckList[index];
+      if (cardNum === deckCard.cardNum) {
+        if (deckCard.count !== "3" || cardObj.name === "Byeah Beast") {
+          // can have unlimited byeah beast
+          copyDeckList[index].count = util.toStringInc(
+            copyDeckList[index].count
+          );
+          setDeckListLength(deckListLength + 1);
+        }
+        return;
+      }
     }
-    
-    return (
-        <DeckListContext.Provider 
-            value={{
-                deckList,
-                challenger,
-                deckListLength,
-                addToDeckList,
-                removeFromDeckList,
-                forceRender,
-                forceRenderDispatch
-            }}
-        >
-            {children}
-        </DeckListContext.Provider>
-    );
+    var parsedCard = {
+      cost: cardObj.cost,
+      cardNum: cardObj.cardNum,
+      name: cardObj.name,
+      imageName: cardObj.deckCardImage,
+      count: "1",
+      isEpic: cardObj.type[0] === "✦",
+    };
+    copyDeckList.push(parsedCard);
+    parsedCard.isEpic && setEpicArray([...epicArray, parsedCard.cardNum]);
+    setDeckListLength(deckListLength + 1);
+    setDeckList([...copyDeckList]);
   };
-  
+
+  const removeFromDeckList = (cardNum: string) => {
+    console.log("context remove hit");
+  };
+
+  return (
+    <DeckListContext.Provider
+      value={{
+        deckList,
+        challenger,
+        deckListLength,
+        epicArray,
+        addToDeckList,
+        removeFromDeckList,
+        forceRender,
+        forceRenderDispatch,
+      }}
+    >
+      {children}
+    </DeckListContext.Provider>
+  );
+};
