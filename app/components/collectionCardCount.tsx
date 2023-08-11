865e8e5ed4147bb613d2962cf69400137f5c335a
Benjamin-Hayduchok <77507585+Benjamin-Hayduchok@users.noreply.github.com>
2023-08-11 16:26:39 -0400

    ben-clean: code to be more concise

--- app/components/collectionCardCount.tsx
+++ app/components/collectionCardCount.tsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from "react";
-import './../../styles/collection.css';
+import "./../../styles/collection.css";
 
-export default function CollectionCardCount(props: {count: string, collectionView: boolean}) {
-    const [count, setCount] = useState("");
+export default function CollectionCardCount(props: {
+  count: string;
+  collectionView: boolean;
+}) {
+  const [count, setCount] = useState("");
 
-    useEffect(() => {
-        setCount(props.count);
-    })
-    if (props.collectionView) {
-        return (
-            <p className="collectionCardCount">{count}</p>
-        )
-        
-    }
-    return (
-        <p className="text-lg justify-center text-center w-full">Collection Count: {count}</p>
-    )
+  useEffect(() => {
+    setCount(props.count);
+  });
+  return (
+    <>
+      {props.collectionView ? (
+        <p className="collectionCardCount">{count}</p>
+      ) : (
+        <p className="text-lg justify-center text-center w-full">
+          Collection Count: {count}
+        </p>
+      )}
+    </>
+  );
 }
